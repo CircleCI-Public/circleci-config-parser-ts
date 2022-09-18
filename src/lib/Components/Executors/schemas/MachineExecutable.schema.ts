3813bbaf1aebe4020048de3784eb6b7ce90fca30
Jaryt <jarytbustard@gmail.com>
2022-09-18 02:26:08 -0400

    feat: accept advanced linux vm resource classes

--- src/lib/Components/Executors/schemas/MachineExecutable.schema.ts
+++ src/lib/Components/Executors/schemas/MachineExecutable.schema.ts
@@ -5,7 +5,19 @@ const MachineExecutableSchema: SchemaObject = {
   type: 'object',
   properties: {
     resource_class: {
-      enum: ['medium', 'large', 'xlarge', '2xlarge'],
+      enum: [
+        'medium',
+        'large',
+        'xlarge',
+        '2xlarge',
+        'arm.medium',
+        'arm.large',
+        'arm.xlarge',
+        'arm.2xlarge',
+        'gpu.nvidia.small',
+        'gpu.nvidia.medium',
+        'gpu.nvidia.large',
+      ],
     },
     description: {
       type: 'string',
@@ -17,12 +29,12 @@ const MachineExecutableSchema: SchemaObject = {
       properties: {
         image: {
           description:
-            'The VM image to use. View [available images](https://circleci.com/docs/2.0/configuration-reference/#available-machine-images). **Note:** This key is **not** supported on the installable CircleCI. For information about customizing machine executor images on CircleCI installed on your servers, see our [VM Service documentation](https://circleci.com/docs/2.0/vm-service).',
+            'The VM image to use. View [available images](https://circleci.com/docs/configuration-reference/#available-linux-machine-images). **Note:** This key is **not** supported on the installable CircleCI. For information about customizing machine executor images on CircleCI installed on your servers, see our [VM Service documentation](https://circleci.com/docs/2.0/vm-service).',
           type: 'string',
         },
         docker_layer_caching: {
           description:
-            'Set to `true` to enable [Docker Layer Caching](https://circleci.com/docs/2.0/docker-layer-caching). Note: You must open a support ticket to have a CircleCI Sales representative contact you about enabling this feature on your account for an additional fee.',
+            'Set to `true` to enable [Docker Layer Caching](https://circleci.com/docs/docker-layer-caching). Note: You must open a support ticket to have a CircleCI Sales representative contact you about enabling this feature on your account for an additional fee.',
           type: 'boolean',
         },
       },
